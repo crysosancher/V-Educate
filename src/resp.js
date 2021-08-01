a058b1701ce519c427621acc21223bd192f67307
crysosancher <vibhupandey.vp@gmail.com>
2021-08-01 21:42:03 +0530

    First commit

---
+++ src/resp.js
@@ -0,0 +1,11 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navlist=document.querySelector('.navlist')
+rightnav=document.querySelector('.rightnav')
+
+burger.addEventListener('click',()=>{
+	navbar.classList.toggle('h-nav-resp');
+	rightnav.classList.toggle('v-class-resp');
+	navlist.classList.toggle('v-class-resp');
+	
+})
